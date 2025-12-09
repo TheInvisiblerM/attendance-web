c62c597289aa6c08bdc70292ca18b888828b232e
TheInvisiblerM <theinvisiblerm6810@gmail.com>
2025-12-09 06:08:07 +0200

    Add files via upload

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,20 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
-// Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyDAfLqHhzTdjG3eH2jaubt1kZ_WCvG3FZw",
-  authDomain: "angles-attendance-web.firebaseapp.com",
-  projectId: "angles-attendance-web",
-  storageBucket: "angles-attendance-web.appspot.com", // صححت الـ storageBucket
-  messagingSenderId: "416084006492",
-  appId: "1:416084006492:web:29c8c3d68d09e49b59b899",
-  measurementId: "G-JY8H6JJZS0"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app); // تصدير Firestore
+export const db = getFirestore(app);
 export const analytics = getAnalytics(app);
